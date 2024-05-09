d2bbe873f218179a752ed0bc28ceda02828e4af2
张大鹏 <1156956636@qq.com>
2024-05-09 13:13:57 +0800

    add: invoke demo

--- study/official_doc/c01_hello_ollama.py
+++ study/official_doc/c01_hello_ollama.py
@@ -1,2 +1,14 @@
+import os
 from langchain_community.llms import Ollama
-llm = Ollama(model="llama2")
+
+# 假设你有一个运行在特定IP地址上的模型服务
+MODEL_SERVICE_IP = "192.168.77.129"
+MODEL_SERVICE_PORT = "11434"
+
+# 设置环境变量，指定代理服务器的IP地址和端口
+os.environ['http_proxy'] = f"http://{MODEL_SERVICE_IP}:{MODEL_SERVICE_PORT}"
+os.environ['https_proxy'] = f"http://{MODEL_SERVICE_IP}:{MODEL_SERVICE_PORT}"
+
+llm = Ollama(model="qwen:4b-chat")
+response = llm.invoke("how can langsmith help with testing?")
+print(response)
